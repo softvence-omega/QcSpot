b22a19dedbb6426f9848965315ea07e8dd89c621
Bakhtiar <bakhtiarfahim360@gmail.com>
2025-03-19 12:10:04 +0600

    updated swiper to slick

--- src/sections/home/Trending.tsx
+++ src/sections/home/Trending.tsx
@@ -1,24 +1,33 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard";
 import { TProduct } from "../../types/product.type";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-import { Autoplay, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
+import Slider from "react-slick";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// Swiper Button styling
-const SwiperNavButtons = () => {
-  const swiper = useSwiper();
+// Custom Next and Prev Buttons
+const NextArrow = (props: any) => {
+  const { onClick } = props;
   return (
-    <section className="flex justify-end items-center gap-5 mt-4 mr-4">
-      <button onClick={() => swiper.slidePrev()}>
-        <FiArrowLeft className="size-8 rounded-full  border-2 text-primary border-primary p-1" />
-      </button>
-      <button onClick={() => swiper.slideNext()}>
-        <FiArrowRight className="size-8 rounded-full  border-2 text-primary border-primary p-1" />
-      </button>
-    </section>
+    <button
+      onClick={onClick}
+      className="absolute right-2 top-1/2 transform -translate-y-1/2 z-10"
+    >
+      <FiArrowRight className="size-8 rounded-full border-2 text-primary border-primary p-1 bg-white shadow-md hover:bg-gray-100 duration-300" />
+    </button>
+  );
+};
+
+const PrevArrow = (props: any) => {
+  const { onClick } = props;
+  return (
+    <button
+      onClick={onClick}
+      className="absolute left-2 top-1/2 transform -translate-y-1/2 z-10"
+    >
+      <FiArrowLeft className="size-8 rounded-full border-2 text-primary border-primary p-1 bg-white shadow-md hover:bg-gray-100 duration-300" />
+    </button>
   );
 };
 
@@ -31,30 +40,37 @@ const Trending = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  // Slick settings
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      { breakpoint: 1440, settings: { slidesToShow: 3 } },
+      { breakpoint: 940, settings: { slidesToShow: 2 } },
+      { breakpoint: 680, settings: { slidesToShow: 1 } },
+    ],
+  };
+
   return (
     <div className="container">
       <h2 className="relative text-center w-fit mx-auto pt-40 md:pt-28 duration-300 text-4xl mb-6">
         Trending Products{" "}
-        <span className="absolute bottom-0 -right-16 text-sm underline text-green-700 hover:text-green-500 duration-300 cursor-pointer">
+        <span className="block mt-3 md:mt-0 md:absolute bottom-0 -right-16 text-sm underline text-green-700 hover:text-green-500 duration-300 cursor-pointer">
           View all
         </span>
       </h2>
 
-      <div className="max-w-7xl mx-auto px-4">
-        <Swiper
-          modules={[Navigation, Autoplay]}
-          spaceBetween={20}
-          slidesPerView={1}
-          loop={true}
-          autoplay={{ delay: 5000, disableOnInteraction: false }}
-          breakpoints={{
-            640: { slidesPerView: 2 },
-            1024: { slidesPerView: 4 },
-          }}
-          className="pb-10"
-        >
+      <div className="max-w-7xl mx-auto px-4 relative">
+        <Slider {...settings} className="pb-10">
           {products.map((product, index) => (
-            <SwiperSlide key={index}>
+            <div key={index} className="px-2">
               <ProductCard
                 image={product.thumbnail}
                 title={product.name}
@@ -62,10 +78,9 @@ const Trending = () => {
                 views={product.views}
                 photos={product.totalImages}
               />
-            </SwiperSlide>
+            </div>
           ))}
-          <SwiperNavButtons />
-        </Swiper>
+        </Slider>
       </div>
     </div>
   );
