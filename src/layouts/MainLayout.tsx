03bd6ec0a0ce36f80630010161a5bba37bde8b43
Bakhtiar <bakhtiarfahim360@gmail.com>
2025-05-03 08:44:33 +0600

    added instagram

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
 import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import insta from "../assets/instagram.svg";
 
 const MainLayout = () => {
   const { setUser } = useAuth();
@@ -10,10 +11,17 @@ const MainLayout = () => {
     if (!localStorage.getItem("user")) setUser(null);
   }, [localStorage]);
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-dark text-black dark:text-white transition-colors duration-200">
+    <div className="relative min-h-screen bg-gray-50 dark:bg-dark text-black dark:text-white transition-colors duration-200">
       <Navbar />
       <Outlet />
       <Footer />
+      <Link
+        className="sticky bottom-5 flex justify-end mr-5 cursor-pointer"
+        to="https://www.instagram.com/qcspot?igsh=MjNtczVlMDlsMmQ4&utm_source=qr"
+        target="_blank"
+      >
+        <img className=" w-10 h-10" src={insta} alt="Instagram" />
+      </Link>
     </div>
   );
 };
